7256c18f42f40a8c4cbfd22162be365be4d1d860
cherish0-0 <jiyu01160@naver.com>
2025-06-18 10:47:53 +0900

    feat(controller): controller 생성 feat(controller): 문자 타입 파라미터를 숫자 타입으로 변경 후 출력

---
+++ src/main/java/springmvc/typeconverter/controller/HelloController.java
@@ -0,0 +1,17 @@
+package springmvc.typeconverter.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data"); // 문자 타입 조회
+        Integer intData = Integer.valueOf(data); // 문자 -> 숫자 타입 변환
+        System.out.println("intData = " + intData);
+        return "ok";
+    }
+}
