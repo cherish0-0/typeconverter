52ab90ba06391e60a65cff75094bfe0b55c63bf9
cherish0-0 <jiyu01160@naver.com>
2025-06-18 10:40:50 +0900

    Initial commit

---
+++ src/test/java/springmvc/typeconverter/TypeconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package springmvc.typeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
