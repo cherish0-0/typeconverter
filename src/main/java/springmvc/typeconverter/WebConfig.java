ee591972378866038fe4c166220018351b8cba3b
cherish0-0 <jiyu01160@naver.com>
2025-06-18 14:57:18 +0900

    chore: 지정 컨버터, 기본 컨버터에 대한 주석 추가

--- src/main/java/springmvc/typeconverter/WebConfig.java
+++ src/main/java/springmvc/typeconverter/WebConfig.java
@@ -11,6 +11,12 @@ import springmvc.typeconverter.converter.StringToIpPortConverter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    /**
+     * 이 컨버터가 없을 때도 잘 동작했던 이유는
+     * 스프링 MVC가 기본적으로 제공하는 컨버터가 있기 때문
+     * 지정 컨버터가 있으면 기본 컨버터보다 우선순위가 높아서 지정 컨버터가 사용됨
+     * @param registry
+     */
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToIntegerConverter());
