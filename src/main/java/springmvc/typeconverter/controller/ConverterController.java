051f6d6bea67f56e3d6c4023eff092e0e956b39b
cherish0-0 <jiyu01160@naver.com>
2025-06-18 18:49:37 +0900

    feat(controller): Form 객체 생성 및 클라이언트에게 화면, 데이터 전달하는 controller 코드 구현 feat(view): th:field와 th:value 각각 컨버전 서비스가 자동 적용되는지 확인하는 뷰 템플릿 구현

--- src/main/java/springmvc/typeconverter/controller/ConverterController.java
+++ src/main/java/springmvc/typeconverter/controller/ConverterController.java
@@ -18,4 +18,27 @@ public class ConverterController {
         return "converter-view";
     }
 
+    @GetMapping("/converter/edit")
+    public String converterForm(Model model) {
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        Form form = new Form(ipPort);
+        model.addAttribute("form", form);
+        return "converter-form";
+    }
+
+    @PostMapping("/converter/edit")
+    public String converterEdit(@ModelAttribute Form form, Model model) {
+        IpPort ipPort = form.getIpPort();
+        model.addAttribute("ipPort", ipPort);
+        return "converter-view";
+    }
+
+    @Data
+    static class Form {
+        private IpPort ipPort;
+
+        public Form(IpPort ipPort) {
+            this.ipPort = ipPort;
+        }
+    }
 }
