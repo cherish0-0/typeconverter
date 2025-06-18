03a6e763b98dd7461ae9d77dda7a3a4a6071aac4
cherish0-0 <jiyu01160@naver.com>
2025-06-18 18:36:48 +0900

    feat(controller): 모델에 객체 담아 뷰 템플릿에 전달하는 컨트롤러 구현 feat(view): 컨버전 서비스 적용한 뷰 템플릿 생성

---
+++ src/main/java/springmvc/typeconverter/controller/ConverterController.java
@@ -0,0 +1,21 @@
+package springmvc.typeconverter.controller;
+
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import springmvc.typeconverter.type.IpPort;
+
+@Controller
+public class ConverterController {
+
+    @GetMapping("/converter-view")
+    public String converterView(Model model) {
+        model.addAttribute("number", 10000);
+        model.addAttribute("ipPort", new IpPort("127.0.0.1", 8080));
+        return "converter-view";
+    }
+
+}
