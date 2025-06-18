2fef4e16996a1cab76b576328b85ffec172e84ef
cherish0-0 <jiyu01160@naver.com>
2025-06-18 10:53:47 +0900

    feat(controller): 스프링 MVC가 제공하는 @RequestParam 사용해 타입 변환 후 출력

--- src/main/java/springmvc/typeconverter/controller/HelloController.java
+++ src/main/java/springmvc/typeconverter/controller/HelloController.java
@@ -2,6 +2,7 @@ package springmvc.typeconverter.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,13 @@ public class HelloController {
         String data = request.getParameter("data"); // 문자 타입 조회
         Integer intData = Integer.valueOf(data); // 문자 -> 숫자 타입 변환
         System.out.println("intData = " + intData);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        // @RequestParam을 사용하여 문자 -> 숫자 타입 변환
+        System.out.println("data = " + data);
         return "ok";
     }
 }
