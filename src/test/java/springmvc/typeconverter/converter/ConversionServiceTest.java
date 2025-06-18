c714b61cd8ca8713c2f107f1ede3a2380ed999b1
cherish0-0 <jiyu01160@naver.com>
2025-06-18 13:48:32 +0900

    chore: DefaultConversionService에 대한 주석 추가

--- src/test/java/springmvc/typeconverter/converter/ConversionServiceTest.java
+++ src/test/java/springmvc/typeconverter/converter/ConversionServiceTest.java
@@ -11,8 +11,8 @@ public class ConversionServiceTest {
     /**
      * 스프링 컨버전 서비스는 Converter를 등록해서 사용할 수 있다.
      * convert 시 등록된 Converter 중 쓰일 수 있는 것을 자동으로 찾아서 변환한다.
-     * 스프링 컨버전 서비스는 컨버터를 등록 및 관리하는 인터페이스와
-     * 변환 로직을 실행하는 인터페이스로 나뉜다.(불필요한 메서드는 모르게 됨)
+     * DefaultConversionService는 컨버터를 등록 및 관리하는 인터페이스(ConversionService)와
+     * 변환 로직을 실행하는 인터페이스(ConverterRegistry)로 나뉜다.(불필요한 메서드는 모르게 됨)
      * 이렇게 인터페이스를 분ㄹ니하는 것을 ISP(Interface Segregation Principle)라고 한다.
      */
     @Test
